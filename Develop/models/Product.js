4c8bbc1cda3a81610d6a75668f368317103ad06b
emilyalv <emilyaalv@gmail.com>
2021-05-02 20:10:57 -0500

    models files update

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,24 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING
+    },    
+    price: {
+      type: DataTypes.DECIMAL(20, 2)
+    },
+    stock: {
+      type: DataTypes.INTEGER    }, 
+
+    category_id: {
+      type: DataTypes.INTEGER
+    }
   },
   {
     sequelize,
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -7,10 +7,19 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',  
+})
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany (Tag, {through: ProductTag});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany (Product, {through: ProductTag});
 
 module.exports = {
   Product,
